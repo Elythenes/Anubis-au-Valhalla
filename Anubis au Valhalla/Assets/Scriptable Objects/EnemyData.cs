6f8c8549926b410ac7d40a6d5bfcb54ca6cf8c0f
Ethan <e.dehay@rubika-edu.com>
2022-10-14 15:29:59 +0200

    Je tiens au jus d'Oscar

--- Anubis au Valhalla/Assets/Scriptable Objects/EnemyData.cs
+++ Anubis au Valhalla/Assets/Scriptable Objects/EnemyData.cs
@@ -7,7 +7,9 @@ public class EnemyData : ScriptableObject
 {
     public float speed;
     public float maxHP;
-    public float damage;
+    public int damage;
+    public int damageAtk2;
+    public float projectileSpeed;
     public int cost;
     public GameObject prefab;
     
@@ -17,13 +19,11 @@ public class EnemyData : ScriptableObject
     public bool isAttacking;
     public bool hasShaked;
     public Vector3 pointToGOFleeing;
-    
-    [Header("Shaman - Déplacements")]
-    public float radiusWonderingShaman;
-    public float forceRepulseShaman;
-    public float distanceMaxPlayerShaman;
-    public float timeFleeingShaman;
-    public float timeFleeingShamanTimer;
+    public float radiusWondering;
+    public float fleeingSpeed;
+    public float disMinPlayer;
+    public float timeFleeing;
+    public float timeFleeingTimer;
    
     [Header("Shaman - Summon")] 
     public float StartUpSummonTime;
@@ -33,19 +33,17 @@ public class EnemyData : ScriptableObject
     public GameObject corbeau;
 
     [Header("Valkyrie - Javalot")]
-    public int puissanceAttaqueJavelot;
-    public float javelotSpeed;
     public float StartUpJavelotTime;
     public float StartUpJavelotTimeTimer;
-    public GameObject projectilJavelot;
+    public GameObject projectileJavelot;
 
     [Header("Valkyrie - Jump")]
     public GameObject indicationFall;
     public GameObject hitboxFall;
     private Vector2 fallPos;
-    public int FallDamage;
-    public float pushForce;
     public bool hasFallen;
+    public int FallDamage;
+    public float fallPushForce;
     public float TriggerJumpTime;
     public float TriggerJumpTimeTimer;     // Le temps que met l'attaque à se tick
     public float JumpTime;
