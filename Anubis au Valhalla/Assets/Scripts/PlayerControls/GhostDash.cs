15ff9af776130f52913ad3b3f1d217a3d4bddb70
OSCAR <95909515+OReynoud@users.noreply.github.com>
2022-11-14 14:46:06 +0100

    essaie deseperement de fix le dash

--- Anubis au Valhalla/Assets/Scripts/PlayerControls/GhostDash.cs
+++ Anubis au Valhalla/Assets/Scripts/PlayerControls/GhostDash.cs
@@ -8,6 +8,7 @@ public class GhostDash : MonoBehaviour
     public float ghostDelay;
     public float ghostDelaySeconds;
     public GameObject ghost;
+    public Sprite ghostSprite;
     public bool activerEffet;
     public List<GameObject> tousLesSprites;
     public Vector3 lastPlayerPos;
@@ -27,9 +28,8 @@ public class GhostDash : MonoBehaviour
             {
                 GameObject currentGhost = Instantiate(ghost, transform.position, transform.rotation);
                 tousLesSprites.Add(currentGhost);
-                Mesh currentSprite = GetComponent<MeshFilter>().mesh;
-                currentGhost.transform.localScale = this.transform.localScale;
-                currentGhost.GetComponent<MeshFilter>().mesh = currentSprite;
+                //currentGhost.transform.localScale = this.transform.localScale;
+                currentGhost.GetComponent<SpriteRenderer>().sprite = ghostSprite;
                 StartCoroutine(Destroyghost(currentGhost));
                 lastPlayerPos = CharacterController.instance.transform.position;
             } 
