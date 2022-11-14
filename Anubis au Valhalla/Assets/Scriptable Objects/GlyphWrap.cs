b6a6799e88527715e24f5784a81b7183702601ba
An0nymaGitFork <felix.sun210302@gmail.com>
2022-11-14 15:52:44 +0100

    feat: système des glyphes intégré
    pas entièrement, la structure est là et MARCHE

--- Anubis au Valhalla/Assets/Scriptable Objects/GlyphWrap.cs
+++ Anubis au Valhalla/Assets/Scriptable Objects/GlyphWrap.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 
-public class GlyphWrap : MonoBehaviour
+[Serializable]
+public class GlyphWrap
 {
     public enum State
     {
--- Anubis au Valhalla/Assets/Scripts/Managers/GlyphInventory.cs
+++ Anubis au Valhalla/Assets/Scripts/Managers/GlyphInventory.cs
@@ -2,27 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class GlyphInventory : MonoBehaviour
 {
     public List<GlyphObject> glyphInventory;
 
-    void Update()
+    public GlyphObject gOTest;
+
+    void Start()
     {
-        
+        AddGlyph(gOTest);
     }
 
-    GlyphWrap AddGlyph(GlyphObject glyphObject, GlyphWrap.State state)
+    void AddGlyph(GlyphObject gO) //faire une fonction pour chaque type de glyphe avec un case plus tôt 
     {
         GlyphWrap wrap = new GlyphWrap();
-
-        if (glyphObject.gType == GlyphObject.GlyphType.BasicStatUp)
-        {
-            state = GlyphWrap.State.Active;
-        }
         
-        return wrap;
+        wrap.glyphObject = gO;
+        wrap.gState = GlyphWrap.State.Active;
+        Debug.Log("glyph set Active");
+
+        if (gO.gPartie == GlyphObject.GlyphPart.Lame)
+        {
+            GlyphManager.instance.arrayLame[gO.gIndex-100] = wrap;
+            Debug.Log("glyph in Manager, array Lame, nom : " + wrap.glyphObject.gNom);
+        }
+        else if (gO.gPartie == GlyphObject.GlyphPart.Manche)
+        {
+            GlyphManager.instance.arrayLame[gO.gIndex-200] = wrap;
+            Debug.Log("glyph in Manager, array Manche, nom : " + wrap.glyphObject.gNom);
+        }
+        else if (gO.gPartie == GlyphObject.GlyphPart.Hampe)
+        {
+            GlyphManager.instance.arrayLame[gO.gIndex-300] = wrap;
+            Debug.Log("glyph in Manager, array Hampe, nom : " + wrap.glyphObject.gNom);
+        }
+        else
+        {
+            Debug.Log("erreur dans l'ajout du glyphe");
+            return;
+        }
+        AddInInventory(wrap.glyphObject);
     }
     
     void AddInInventory(GlyphObject glyphObject)
