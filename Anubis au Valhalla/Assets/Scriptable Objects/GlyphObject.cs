eeddb3fef098109f7042bd26d7fd712356c49178
An0nymaGitFork <felix.sun210302@gmail.com>
2022-11-14 10:55:00 +0100

    fix: rangement des scripts

--- Anubis au Valhalla/Assets/Scriptable Objects/GlyphObject.cs
+++ Anubis au Valhalla/Assets/Scriptable Objects/GlyphObject.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Glyphe" ,menuName = "Glyphe System/GlypheObject")]
+[CreateAssetMenu(fileName = "Glyph" ,menuName = "Glyph System/GlyphObject")]
 
-public class GlypheObject : ScriptableObject
+public class GlyphObject : ScriptableObject
 {
     [Header("GENERAL")]
     public string nom;
-    public int glypheIndex;
+    public int glyphIndex;
     [TextArea(10,20)] public string description;
 
     [Header("GRAPH")]
-    public Texture icone;
+    public Texture icon;
     
     
 }
