2066cbac46466d89fd958462b1c2a6805101ff5a
An0nymaGitFork <felix.sun210302@gmail.com>
2022-11-18 12:00:03 +0100

    fix: re-architecture du GlyphSystem
    changement de 1 sO par type de glypheObject à 1 sO pour tous les types de glypheObjects

--- Anubis au Valhalla/Assets/Scriptable Objects/GlyphsFolder/GlyphObject.cs
+++ Anubis au Valhalla/Assets/Scriptable Objects/GlyphsFolder/GlyphObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//[CreateAssetMenu(fileName = "Glyph" ,menuName = "Glyph System/GlyphObject")]
+[CreateAssetMenu(fileName = "Glyph" ,menuName = "Glyph System/GlyphObject")]
 public class GlyphObject : ScriptableObject
 {
     [Header("GENERAL")]
@@ -14,13 +14,48 @@ public class GlyphObject : ScriptableObject
     [TextArea(10,20)] public string description;
     //[TextArea(5, 10)] public string citation;
     //public GlyphElement element;
+    [Range(1, 4)] public int tier = 1;
     
-
     [Header("GRAPH")]
     public Texture icone;
     public Texture iconeElement; // (visible à côté / dans l'icone) pour indiquer l'élément dans le Shop
     public Texture fondElement; // (visible dans l'Inventaire) pour savoir s'il est associé à un élément 
 
+    [Header("BASIC STAT UP")] 
+    public bool isBasicStatUp = false;
+    public AnubisStat anubisStat = AnubisStat.None;
+    public float bonusBasicStat = 5f;
+    
+    [Header("SITUATIONAL STAT UP")]
+    public bool isSituationStatUp = false;
+    public int valeurPourLeHeader1;
+    
+    [Header("ELEMENTAL")] 
+    public bool isElemental = false;
+    public GlyphElement glyphElement;
+    
+    [Header("ADDITIONAL EFFECT")]
+    public bool isAdditionalEffect = false;
+    public int valeurPourLeHeader2;
+    
+    [Header("TRIGGER EFFECT")]
+    public bool isTriggerEffect = false;
+    public bool isTriggerActive = false;
+    
+    [Header("CHARGE BASED")] 
+    public bool isCharge = false;
+    public int chargeBase = 0;
+    public int chargeNumber = 10;
+
+    [Header("TIME BASED")] 
+    public bool isTimeBased = false;
+    public float cooldownBeforeEffect = 5f;
+    
+    [Header("BOOL EFFECT")]
+    public bool isBoolEffect = false;
+    public bool isEffectActive = true;
+    
+    
     public enum GlyphPart
     {
         Lame,
@@ -30,9 +65,9 @@ public class GlyphObject : ScriptableObject
 
     public enum GlyphType
     {
-        BasicStatUp,
-        SituationalStatUp,
-        Elemental,
+        BasicStatUp = 1,
+        SituationalStatUp = 2,
+        Elemental = 3,
         AdditionalEffect,
         TriggerEffect,
         Charge,
