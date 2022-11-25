3c222d283c2b9c27f30c48813ed252321297a54e
An0nymaGitFork <felix.sun210302@gmail.com>
2022-11-25 15:04:10 +0100

    feat: Potion system terminé

--- Anubis au Valhalla/Assets/Scriptable Objects/Potion/PotionObject.cs
+++ Anubis au Valhalla/Assets/Scriptable Objects/Potion/PotionObject.cs
@@ -4,33 +4,41 @@ using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.VFX;
 
-
 [CreateAssetMenu(fileName = "Potion" ,menuName = "System/Potion System/PotionObject")]
 public class PotionObject : ScriptableObject
 {
     [Header("GENERAL")] 
+    public PotionType type;
     public string nom;
     [TextArea(6, 20)] public string description;
-    [TextArea(3, 20)] public string citation;
-    public int index;
+    [TextArea(2, 20)] public string citation;
 
     [Header("GAME DESIGN")] 
+    public int index;
     public int prix;
-    
+
     [BoxGroup("BUFFS")] public int damage;
-    [BoxGroup("BUFFS")] public int attackSpeed;
+    //[BoxGroup("BUFFS")] public int attackSpeed;
     [BoxGroup("BUFFS")] public int heal;
     [BoxGroup("BUFFS")] public int armor;
     [BoxGroup("BUFFS")] public int speed;
     [BoxGroup("BUFFS")] public int magicForce;
     
-    [BoxGroup("DEBUFFS")] public int w_armor;
-    [BoxGroup("DEBUFFS")] public int w_magicForce;
-    
+    [BoxGroup("DEBUFFS")] public int wArmor;
+    [BoxGroup("DEBUFFS")] public int wMagicForce;
+
     [Header("GRAPH")]
     public Texture sprite;
     //public VFX vfx;
 
+    public enum PotionType
+    {
+        StatBasicPotion,
+        StatSpecificPotion,
+        SpecialPotion,
+        SpecialItem
+    }
+    
 
     /*[Header("GENERAL")]
     public PotionType type;
@@ -43,14 +51,6 @@ public class PotionObject : ScriptableObject
     public float cooldown;
     public bool canCast;
     public int valeurRestored;
-    
-    
-    
-    public enum PotionType
-{
-    InstantEffect,
-    EffectOverTime
-}
     
     */
 }
--- Anubis au Valhalla/Assets/Scripts/Managers/PotionManager.cs
+++ Anubis au Valhalla/Assets/Scripts/Managers/PotionManager.cs
@@ -1,144 +1,72 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class PotionManager : MonoBehaviour
 {
-    
-   /* [Header("GENERAL")] 
-    public static PotionManager instance;
-    public AnubisCurrentStats anubis;
-    public GameObject targetUser;
-    public KeyCode potionKey;
-    public bool canUsePotion;
-    
-    [Header("POTION SLOT")]
-    public SpellObject containerA;
-    public float cooldownSlot;
-    public float cooldownSlotTimer;
+   public static PotionManager Instance;
 
-    [Header("SPELL HIDDEN VAR")] 
-   // [SerializeField] public InstantEffectObject spellIEo;
-    [SerializeField] public EffectOverTimeObject spellEOTo;
-    public bool isPotionFill = false;
+   public KeyCode usePotion = KeyCode.A;
+   [Expandable] public PotionObject currentPotion;
 
-    
-     //Fonctions Système *************************************************************************************************
-    
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        canUsePotion = true;
-    }
 
-    private void Start()
-    {
-        containerA = null;
-    }
+   private void Awake()
+   {
+      if (Instance == null)
+      {
+         Instance = this;
+      }
+   }
 
-    void Update()
-    {
+   private void Update()
+   {
+      if (Input.GetKeyDown(usePotion))
+      {
+         if (currentPotion.type == PotionObject.PotionType.SpecialItem)
+         {
+            Debug.Log("Je ne peux pas boire une chose qui n'est pas une potion");
+         }
+         else
+         {
+            DrinkPotion(currentPotion);
+         }
+      }
+   }
 
-        if (isPotionFill)
-        {
-            SpellCooldownCalcul(ConvertSpellIndex(containerA));
-            if (Input.GetKeyDown(potionKey))
-            {
-                Debug.Log("spell 1 input");
-                UseSpellSlot1(ConvertSpellIndex(containerA));
-            }
-        }
-    }
-
-    
-    //Spell System *****************************************************************************************************
-    
-    
-    private enum PotionNumber
-    {
-       Heal = 0,
-       AttackBuff = 1,
-    }
-
-    PotionNumber ConvertSpellIndex(PotionObject potionObject)
-    {
-        var x = potionObject.potionIndex;
-        Debug.Log("utilisation du spell index "+ x);
-        return (PotionNumber)x;
-    }
-
-    void UseSpellSlot1(PotionNumber potionNumber, ScriptableObject potionObject)
-    {
-        switch (potionNumber)
-            {
-                case PotionNumber.Heal:
-                  // HealPotion(containerA);
-                    break;
-                
-                case PotionNumber.AttackBuff:
-                    AttackBuffPotion(containerA);
-                    break;
-            }
-    }
-    
-    void SpellCooldownCalcul(PotionNumber potionNumber) //pour calculer les CD de chaque spell (la fonction DEVRAIT se lancer que si on a le spell en question)
-    {
-        switch (potionNumber)
-        {
-            case PotionNumber.Heal:
-                spellIEo = containerA;
-                cooldownSlot = spellIEo.cooldown;
-                
-                if (spellIEo.cooldownTimer < spellIEo.cooldown && !spellIEo.canCast) //cooldown de la Fireball
-                {
-                    spellIEo.cooldownTimer += Time.deltaTime;
-                    cooldownSlotTimer = spellIEo.cooldownTimer;
-                }
-                else if (spellIEo.cooldownTimer > spellIEo.cooldown)
-                {
-                    spellIEo.canCast = true;
-                    spellIEo.cooldownTimer = 0;
-                    cooldownSlotTimer = 0;
-                }
-                break;
-        }
-    }
-    
-    
-    //Script des spells ************************************************************************************************
-
-    IEnumerator EffectDuration(EffectOverTimeObject spellEOTo)
-    {
-        yield return new WaitForSeconds(spellEOTo.buffDuration);
-        anubis.comboDamage[1] -= spellEOTo.buffAmount;
-        anubis.comboDamage[2] -= spellEOTo.buffAmount;
-        anubis.comboDamage[3] -= spellEOTo.buffAmount;
-    }
-    
-    void HealPotion(InstantEffectObject spellIEo)
-    {
-        if (spellIEo.canCast)
-        {
-            cooldownSpellBar.instance.SetCooldownMax1();
-            spellIEo.canCast = false;
-            anubis.vieActuelle += spellIEo.valeurRestored;
-        }
-    }
-    
-    void AttackBuffPotion(EffectOverTimeObject spellEOTo)
-    {
-        if (spellEOTo.canCast)
-        {
-            cooldownSpellBar.instance.SetCooldownMax1();
-            spellEOTo.canCast = false;
-            anubis.comboDamage[1] += spellEOTo.buffAmount;
-            anubis.comboDamage[2] += spellEOTo.buffAmount;
-            anubis.comboDamage[3] += spellEOTo.buffAmount;
-            StartCoroutine(EffectDuration(spellEOTo));
-        }
-    }*/
+   void DrinkPotion(PotionObject glouglou)
+   {
+      if (glouglou.type == PotionObject.PotionType.StatBasicPotion 
+          || glouglou.type == PotionObject.PotionType.StatSpecificPotion)
+      {
+         for (int i = 0; i < 3; i++)
+         {
+            AnubisCurrentStats.instance.comboDamage[i] += glouglou.damage;
+         }
+         AnubisCurrentStats.instance.thrustDamage += glouglou.damage;
+         //DamageManager.instance.Heal(glouglou.heal);
+         AnubisCurrentStats.instance.damageReduction += glouglou.armor;
+         if (glouglou.wArmor != 0)
+         {
+            AnubisCurrentStats.instance.damageReduction /= glouglou.wArmor;
+         }
+         if (glouglou.speed != 0)
+         {
+            AnubisCurrentStats.instance.speedX *= glouglou.speed;
+            AnubisCurrentStats.instance.speedY *= glouglou.speed;
+         }
+         //Fonction avec la MagicForce
+         Debug.Log("drink potion et ajout de stat");
+      }
+      else if (glouglou.type == PotionObject.PotionType.SpecialPotion)
+      {
+         switch (glouglou.index)
+         {
+            case 1:
+               break;
+         }
+      }
+   }
 }
