c4ffabd028f6c8de3d8a28dac377a6c95bfb5c83
An0nymaGitFork <felix.sun210302@gmail.com>
2022-11-28 10:36:17 +0100

    feat: début d'ajout des glyphes, potion ramassable

---
+++ Anubis au Valhalla/Assets/Scripts/Potions Scripts/PotionRepository.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PotionRepository : MonoBehaviour
+{
+    public static PotionRepository Instance;
+    
+    public List<PotionObject> potionsList;
+    public PotionObject potionInside;
+
+    public bool isMoving;
+    
+    [Header("Force Au Spawn")]
+    [SerializeField] private float force = 3f;
+    [SerializeField] private float deceleration = 0.3f;
+    public float timer;
+    public Rigidbody2D rb;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        int numberGot = Random.Range(0, potionsList.Count);
+        potionInside = potionsList[numberGot];
+        
+        if (isMoving)
+        {
+            Vector2 explode = new Vector2(Random.Range(-force, force), Random.Range(-force, force));
+            rb.AddForce(explode, ForceMode2D.Impulse);
+            rb.drag = deceleration;
+        }
+    }
+    
+    private void Update()
+    {
+        if (timer >= 0)
+        {
+            rb.velocity -= rb.velocity * 0.01f;
+            timer -= Time.deltaTime;
+        }
+    }
+    
+}
