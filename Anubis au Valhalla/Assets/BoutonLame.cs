c315ca71cb88bfeac975ba5e725346cc92683dcb
Ethan Dehay <e.dehay@rubika-edu.com>
2022-12-04 03:01:25 +0100

    Encore des améliorations du shop

--- Anubis au Valhalla/Assets/BoutonLame.cs
+++ Anubis au Valhalla/Assets/BoutonLame.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
-public class BoutonConsomables : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class BoutonLame : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Animator anim;
-    
+
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        anim.SetBool("SelectConsomable",true);
+        anim.SetBool("LameUp",true);
+        anim.SetBool("LameDown",false);
         anim.SetBool("Idle",false);
     }
     
     public void OnPointerExit(PointerEventData pointerEventData)
     {
-        anim.SetBool("SelectConsomable",false);
+        anim.SetBool("LameDown",true);
+        anim.SetBool("LameUp",false);
         anim.SetBool("Idle",true);
     }
 }
