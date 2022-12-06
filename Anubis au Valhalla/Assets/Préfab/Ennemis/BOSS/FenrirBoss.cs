6444ce1805d5342ed3033571bb642c7d4d9c0e37
OSCAR <95909515+OReynoud@users.noreply.github.com>
2022-12-06 18:04:45 +0100

    boss: je track la position générale du joueur pour savoir quel genre d'attaques utiliser

--- Anubis au Valhalla/Assets/Préfab/Ennemis/BOSS/FenrirBoss.cs
+++ Anubis au Valhalla/Assets/Préfab/Ennemis/BOSS/FenrirBoss.cs
@@ -1,13 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FenrirBoss : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController player;
+    public static FenrirBoss instance;
+    public bool center;
+    public bool left;
+    public bool right;
+    public bool back;
+    
+    public enum State
     {
-        
+        Idle,
+        Exposed,
+        Attacking
+    }
+    
+    public enum Phase
+    {
+        Normal,
+        Furious
+    }
+    public enum ZoneType
+    {
+        Center,
+        Left,
+        Right,
+        Back,
+    }
+    
+    // Start is called before the first frame update
+    public virtual void Awake()
+    {
+        if (instance != null)
+        {
+            DestroyImmediate(instance);
+        }
+
+        instance = this;
+    }
+
+    private void Start()
+    {
+        player = CharacterController.instance;
     }
 
     // Update is called once per frame
