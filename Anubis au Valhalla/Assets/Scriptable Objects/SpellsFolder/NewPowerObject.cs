4353ed31b0d87f8cc64855e2713127bad63bd28b
An0nymaGitFork <felix.sun210302@gmail.com>
2022-12-12 17:36:28 +0100

    feat: rework du spell system en power system

---
+++ Anubis au Valhalla/Assets/Scriptable Objects/SpellsFolder/NewPowerObject.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPowerObject : ScriptableObject
+{
+    public int upgradeValue = 1;
+    
+
+}
+
+public enum NewPowerType
+{
+    Power1,
+    Power2,
+}
