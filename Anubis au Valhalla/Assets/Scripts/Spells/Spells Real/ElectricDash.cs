7176bd3c420a0f3cafc381e0f5a273ac82e7d090
Ethan Dehay <e.dehay@rubika-edu.com>
2023-01-23 03:39:11 +0100

    Tentative d'integration des VFX de spell + fix cinématique d'intro + fix salles + j'ai envie de caner

--- Anubis au Valhalla/Assets/Scripts/Spells/Spells Real/ElectricDash.cs
+++ Anubis au Valhalla/Assets/Scripts/Spells/Spells Real/ElectricDash.cs
@@ -3,14 +3,46 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class ElectricDash : MonoBehaviour
 {
     public NewPowerManager manager;
     public CooldownPowerBar cooldown;
+    public GameObject VFX;
+    public VisualEffect realVFX;
 
     private void Start()
     {
+        switch (CharacterController.instance.facing)
+        {
+            case CharacterController.LookingAt.Nord:
+                VFX.transform.rotation = new Quaternion(0,0,90,0);
+                break;
+            case CharacterController.LookingAt.NordEst:
+                VFX.transform.rotation = new Quaternion(0,0,45,0);
+                break;
+            case CharacterController.LookingAt.NordOuest:
+                VFX.transform.rotation = new Quaternion(0,0,135,0);
+                break;
+            case CharacterController.LookingAt.Sud:
+                VFX.transform.rotation = new Quaternion(0,0,-90,0);              
+                break;
+            case CharacterController.LookingAt.SudEst:
+                VFX.transform.rotation = new Quaternion(0,0,-45,0);  
+                break;
+            case CharacterController.LookingAt.SudOuest:
+                VFX.transform.rotation = new Quaternion(0,0,-135,0); 
+                break;
+            case CharacterController.LookingAt.Est:
+                VFX.transform.rotation = new Quaternion(0,0,0,0);     
+                break;
+            case CharacterController.LookingAt.Ouest:
+                VFX.transform.rotation = new Quaternion(0, 0, 180, 0);
+                break;
+        }
+        
+        realVFX.Play();
         cooldown = GameObject.Find("Spells - Consommable").GetComponent<CooldownPowerBar>();
         manager = GameObject.Find("NewPowerManager").GetComponent<NewPowerManager>();
     }
