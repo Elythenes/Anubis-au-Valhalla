d642f66f66729af9bfcd16c9675e75b429df8c17
Ethan Dehay <e.dehay@rubika-edu.com>
2023-01-23 01:18:02 +0100

    Fix UI + salles

--- Anubis au Valhalla/Assets/Scripts/UI/PauseMenu.cs
+++ Anubis au Valhalla/Assets/Scripts/UI/PauseMenu.cs
@@ -9,11 +9,17 @@ public class PauseMenu : MonoBehaviour
     public void RestartRun()
     {
         SceneManager.LoadScene("Test");
+        SoundManager.instance.audioSource.Stop();
+        SoundManager.instance.ChangeToZone1();
+        SoundManager.instance.PlayZone1();
     }
 
     public void BackHub()
     {
         SceneManager.LoadScene("Hub");
+        SoundManager.instance.audioSource.Stop();
+        SoundManager.instance.ChangeToHub();
+        SoundManager.instance.PlayHub();
     }
 
     public void Quit()
